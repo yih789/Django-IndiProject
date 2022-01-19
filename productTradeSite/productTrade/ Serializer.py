4bf48dde015a77fd7cb73c8ca1dea833f02a4bb1
yih789 <yih789@naver.com>
2022-01-19 21:55:17 +0900

    Add "serialize.py"

--- productTradeSite/productTrade/ Serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import  serializers
-from .models import Content
-
-class ContentSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(use_url=True)
-    
-    class Meta:
-        model = Content
-        fields = ('title', 'text', 'image')
